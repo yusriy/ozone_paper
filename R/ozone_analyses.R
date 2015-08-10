389a73b1f23575f5985ad0e381f2bb1f708ea4a2
yusriy <yusriyp@gmail.com>
2015-08-10 23:50:31 +0800

    Some corrections and added mean and sd results

--- R/ozone_analyses.R
+++ R/ozone_analyses.R
@@ -44,11 +44,11 @@ CA0038ws <- TheilSen(selectByDate(CA0038,year=2003:2013),statistic = 'median',
 
 ## RH mainland
 CA0003RH <- TheilSen(selectByDate(CA0003,year=2003:2013),statistic = 'median',
-                     pollutant='ws',main='WMKB',deseason=TRUE,dec.place=4,
+                     pollutant='RH_met',main='WMKB',deseason=TRUE,dec.place=4,
                      shade='white',fontsize=14,ylab='RH',type='monsoon')
 ## RH island
 CA0038RH <- TheilSen(selectByDate(CA0038,year=2003:2013),statistic = 'median',
-                     pollutant='ws',main='WMKP',deseason=TRUE,dec.place=4,
+                     pollutant='RH_met',main='WMKP',deseason=TRUE,dec.place=4,
                      shade='white',fontsize=14,ylab='RH',type='monsoon')
 
 ## WD mainland
@@ -60,6 +60,104 @@ CA0038wd <- TheilSen(selectByDate(CA0038,year=2003:2013),statistic = 'median',
                      pollutant='wd',main='WMKP',deseason=TRUE,dec.place=4,
                      shade='white',fontsize=14,ylab='WD',type='monsoon')
 
-## Results: basically, there are no yearly changes in T, RH, WS, and WD from 2003-2013
+## Results: basically, there are no yearly changes in (at the 95% sig. level)
+## T, RH, WS, and WD from 2003-2013
 
+## O3 mainland
+CA0003o3 <- TheilSen(selectByDate(CA0003,year=2003:2013),statistic = 'median',
+                     pollutant='o3',main='WMKB',deseason=TRUE,dec.place=4,
+                     shade='white',fontsize=14,ylab='O3',type='monsoon')
+## O3 island
+CA0038o3 <- TheilSen(selectByDate(CA0038,year=2003:2013),statistic = 'median',
+                     pollutant='o3',main='WMKP',deseason=TRUE,dec.place=4,
+                     shade='white',fontsize=14,ylab='O3',type='monsoon')
 
+## NO2 mainland
+CA0003no2 <- TheilSen(selectByDate(CA0003,year=2003:2013),statistic = 'median',
+                     pollutant='no2',main='WMKB',deseason=TRUE,dec.place=4,
+                     shade='white',fontsize=14,ylab='NO2',type='monsoon')
+## NO2 island
+CA0038no2 <- TheilSen(selectByDate(CA0038,year=2003:2013),statistic = 'median',
+                     pollutant='no2',main='WMKP',deseason=TRUE,dec.place=4,
+                     shade='white',fontsize=14,ylab='NO2',type='monsoon')
+
+#### Means and SD of met for different Monsoons ####
+## Northeast Monsoon
+# Mainland T NEM
+mean(CA0003$T_met[which(CA0003$monsoon == 'NEM')],na.rm=TRUE)
+sd(CA0003$T_met[which(CA0003$monsoon == 'NEM')],na.rm=TRUE)
+# Island T NEM
+mean(CA0038$T_met[which(CA0038$monsoon == 'NEM')],na.rm=TRUE)
+sd(CA0038$T_met[which(CA0038$monsoon == 'NEM')],na.rm=TRUE)
+# Mainland RH NEM
+mean(CA0003$RH_met[which(CA0003$monsoon == 'NEM')],na.rm=TRUE)
+sd(CA0003$RH_met[which(CA0003$monsoon == 'NEM')],na.rm=TRUE)
+# Island RH NEM
+mean(CA0038$RH_met[which(CA0038$monsoon == 'NEM')],na.rm=TRUE)
+sd(CA0038$RH_met[which(CA0038$monsoon == 'NEM')],na.rm=TRUE)
+# Mainland WS NEM
+mean(CA0003$ws[which(CA0003$monsoon == 'NEM')],na.rm=TRUE)
+sd(CA0003$ws[which(CA0003$monsoon == 'NEM')],na.rm=TRUE)
+# Island WS NEM
+mean(CA0038$ws[which(CA0038$monsoon == 'NEM')],na.rm=TRUE)
+sd(CA0038$ws[which(CA0038$monsoon == 'NEM')],na.rm=TRUE)
+
+## Spring Transitional Inter-monsoon
+# Mainland T STM
+mean(CA0003$T_met[which(CA0003$monsoon == 'STM')],na.rm=TRUE)
+sd(CA0003$T_met[which(CA0003$monsoon == 'STM')],na.rm=TRUE)
+# Island T STM
+mean(CA0038$T_met[which(CA0038$monsoon == 'STM')],na.rm=TRUE)
+sd(CA0038$T_met[which(CA0038$monsoon == 'STM')],na.rm=TRUE)
+# Mainland RH STM
+mean(CA0003$RH_met[which(CA0003$monsoon == 'STM')],na.rm=TRUE)
+sd(CA0003$RH_met[which(CA0003$monsoon == 'STM')],na.rm=TRUE)
+# Island RH STM
+mean(CA0038$RH_met[which(CA0038$monsoon == 'STM')],na.rm=TRUE)
+sd(CA0038$RH_met[which(CA0038$monsoon == 'STM')],na.rm=TRUE)
+# Mainland WS STM
+mean(CA0003$ws[which(CA0003$monsoon == 'STM')],na.rm=TRUE)
+sd(CA0003$ws[which(CA0003$monsoon == 'STM')],na.rm=TRUE)
+# Island WS STM
+mean(CA0038$ws[which(CA0038$monsoon == 'STM')],na.rm=TRUE)
+sd(CA0038$ws[which(CA0038$monsoon == 'STM')],na.rm=TRUE)
+
+## Southwest Monsoon
+# Mainland T SWM
+mean(CA0003$T_met[which(CA0003$monsoon == 'SWM')],na.rm=TRUE)
+sd(CA0003$T_met[which(CA0003$monsoon == 'SWM')],na.rm=TRUE)
+# Island T SWM
+mean(CA0038$T_met[which(CA0038$monsoon == 'SWM')],na.rm=TRUE)
+sd(CA0038$T_met[which(CA0038$monsoon == 'SWM')],na.rm=TRUE)
+# Mainland RH SWM
+mean(CA0003$RH_met[which(CA0003$monsoon == 'SWM')],na.rm=TRUE)
+sd(CA0003$RH_met[which(CA0003$monsoon == 'SWM')],na.rm=TRUE)
+# Island RH SWM
+mean(CA0038$RH_met[which(CA0038$monsoon == 'SWM')],na.rm=TRUE)
+sd(CA0038$RH_met[which(CA0038$monsoon == 'SWM')],na.rm=TRUE)
+# Mainland WS SWM
+mean(CA0003$ws[which(CA0003$monsoon == 'SWM')],na.rm=TRUE)
+sd(CA0003$ws[which(CA0003$monsoon == 'SWM')],na.rm=TRUE)
+# Island WS SWM
+mean(CA0038$ws[which(CA0038$monsoon == 'SWM')],na.rm=TRUE)
+sd(CA0038$ws[which(CA0038$monsoon == 'SWM')],na.rm=TRUE)
+
+## Fall Transitional Inter-monsoon
+# Mainland T FTM
+mean(CA0003$T_met[which(CA0003$monsoon == 'FTM')],na.rm=TRUE)
+sd(CA0003$T_met[which(CA0003$monsoon == 'FTM')],na.rm=TRUE)
+# Island T FTM
+mean(CA0038$T_met[which(CA0038$monsoon == 'FTM')],na.rm=TRUE)
+sd(CA0038$T_met[which(CA0038$monsoon == 'FTM')],na.rm=TRUE)
+# Mainland RH FTM
+mean(CA0003$RH_met[which(CA0003$monsoon == 'FTM')],na.rm=TRUE)
+sd(CA0003$RH_met[which(CA0003$monsoon == 'FTM')],na.rm=TRUE)
+# Island RH FTM
+mean(CA0038$RH_met[which(CA0038$monsoon == 'FTM')],na.rm=TRUE)
+sd(CA0038$RH_met[which(CA0038$monsoon == 'FTM')],na.rm=TRUE)
+# Mainland WS FTM
+mean(CA0003$ws[which(CA0003$monsoon == 'FTM')],na.rm=TRUE)
+sd(CA0003$ws[which(CA0003$monsoon == 'FTM')],na.rm=TRUE)
+# Island WS FTM
+mean(CA0038$ws[which(CA0038$monsoon == 'FTM')],na.rm=TRUE)
+sd(CA0038$ws[which(CA0038$monsoon == 'FTM')],na.rm=TRUE)
