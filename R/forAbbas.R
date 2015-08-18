d6be89266a339718da7ff2e4861c387b80ee0298
yusriy <yusriyp@gmail.com>
2015-08-18 15:07:43 +0800

    Added 8 hours max in forAbbas.R script

--- R/forAbbas.R
+++ R/forAbbas.R
@@ -21,9 +21,35 @@ CA0038_abbas <- CA0038_abbas[,-c(3,4,5,6,7,8,11,12,13,14,18,19,20,21,23)]
 # Replace all NAs to zeros
 CA0038_abbas[is.na(CA0038_abbas)] <- 0
 
+# 8 hour max
+# mainland
+CA0003_abbas8 <- selectByDate(CA0003_8h,start='1/1/2003',end='31/12/2013')
+CA0003_abbas8$date <- CA0003_abbas8$date + 28800
+CA0003_abbas8 <- CA0003_abbas8[,-c(3,4,5,6,7,8,11,12,13,14,18,19,20,21,23)]
+# Replace all NAs to zeros
+CA0003_abbas8[is.na(CA0003_abbas8)] <- 0
+# mainland
+CA0009_abbas8 <- selectByDate(CA0009_8h,start='1/1/2003',end='31/12/2013')
+CA0009_abbas8$date <- CA0009_abbas8$date + 28800
+CA0009_abbas8 <- CA0003_abbas8[,-c(3,4,5,6,7,8,11,12,13,14,18,19,20,21,23)]
+# Replace all NAs to zeros
+CA0009_abbas8[is.na(CA0009_abbas8)] <- 0
+
+# island
+CA0038_abbas8 <- selectByDate(CA0038_8h,start='1/1/2003',end='31/12/2013')
+CA0038_abbas8$date <- CA0038_abbas8$date + 28800
+CA0038_abbas8 <- CA0038_abbas8[,-c(3,4,5,6,7,8,11,12,13,14,18,19,20,21,23)]
+# Replace all NAs to zeros
+CA0038_abbas8[is.na(CA0038_abbas8)] <- 0
+
 # Combine all files by rows
 data_abbas <- rbind(CA0003_abbas,CA0009_abbas,CA0038_abbas)
 write.table(CA0003_abbas,file='data/CA0003_abbas.csv',sep=',',row.names = FALSE)
 write.table(CA0009_abbas,file='data/CA0009_abbas.csv',sep=',',row.names = FALSE)
 write.table(CA0038_abbas,file='data/CA0038_abbas.csv',sep=',',row.names = FALSE)
+
+write.table(CA0003_abbas8,file='data/CA0003_abbas8.csv',sep=',',row.names = FALSE)
+write.table(CA0009_abbas8,file='data/CA0009_abbas8.csv',sep=',',row.names = FALSE)
+write.table(CA0038_abbas8,file='data/CA0038_abbas8.csv',sep=',',row.names = FALSE)
+
 write.table(data_abbas,file='data/data_abbas.csv',sep=',',row.names=FALSE)
